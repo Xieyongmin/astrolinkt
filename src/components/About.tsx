02bd1202a88c482c5fb5b189153fb541253d767e
Joe <1147782+Xieyongmin@users.noreply.github.com>
2024-03-25 00:47:43 +0800

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,21 +11,13 @@ export default () => {
 
         <div className="flex flex-col items-start gap-2">
           <h1 className="font-medium text-lg lg:text-xl">flamrdevs</h1>
-          <h2 className="font-medium text-2xl lg:text-3xl">UI/UX designer</h2>
+          <!-- <h2 className="font-medium text-2xl lg:text-3xl">UI/UX designer</h2> -->
         </div>
       </div>
 
-      <div>
-        I am a UI/UX designer from Indonesia, specializing in creating user-centric and visually appealing digital experiences. With a focus
-        on seamless and enjoyable interactions, I aim to enhance the overall user experience through strategic design solutions.
-      </div>
-
-      <div className="flex self-stretch justify-end">
-        <Button as="a" href="https://github.com/flamrdevs/astrolinkt" target="_blank" aria-label="GitHub">
-          <Simple.IconGitHub />
-          GitHub
-        </Button>
-      </div>
+      <!-- <div> -->
+        <!-- 简介 -->
+      <!-- </div> -->
     </div>
   );
 };
